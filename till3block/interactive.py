d5427845670b9ec9ec21ef95b29ad1715801d781
Ekaterina Novikova <ekaterina.novikova@telefonica.com>
2021-01-26 19:31:00 +0100

    with methods

--- till3block/interactive.py
+++ till3block/interactive.py
@@ -3,52 +3,81 @@ import csv
 import pandas as pd
 import argparse
 
-con = psycopg2.connect(database="aland", user="azubi",
-                       password="a1t1AZUBI", host="localhost", port="5432")
-print("Database opened successfully", end='\n\n')
 
-cursor = con.cursor()
+def connection():
+    global con, cursor
+    con = psycopg2.connect(database="aland", user="azubi",
+                           password="a1t1AZUBI", host="localhost", port="5432")
+    print("Database opened successfully", end='\n\n')
+    cursor = con.cursor()
 
-# interactive
-parameter = input(
-    "Type:\n - name of continent or \n - first letter of land (A-D) or \n - 'all' for the hole table\n: ")
-print()
-if len(parameter) < 3:
-    query = "SELECT * FROM land WHERE name LIKE \'" + parameter + "%\'"
-elif len(parameter) == 3:
-    query = "SELECT * FROM land"
-else:
-    query = "SELECT * FROM land WHERE kontinent = \'" + parameter + "\'"
-print("Query is: " + query)
-print("Here the results:")
+
+connection()
+
+
+def interactive():
+    global parameter, query
+    parameter = input(
+        "Type:\n - name of continent or \n - first letter of land (A-D) or \n - 'all' for the hole table\n: ")
+    print()
+    if len(parameter) < 3:
+        query = "SELECT * FROM land WHERE name LIKE \'" + parameter + "%\'"
+    elif len(parameter) == 3:
+        query = "SELECT * FROM land"
+    else:
+        query = "SELECT * FROM land WHERE kontinent = \'" + parameter + "\'"
+    print("Query is: " + query)
+
+
+interactive()
 
 # Durchlaufen der Ergebnisse
-cursor.execute(query)
-row = cursor.fetchone()
-while (row != None):
-    print(row)
+
+
+def here_the_results():
+    global row
+    print("Here the results:")
+    cursor.execute(query)
     row = cursor.fetchone()
+    while (row != None):
+        print(row)
+        row = cursor.fetchone()
+
+
+here_the_results()
 
 # export to csv without header
-answer = input("Do you want to save the results in csv document? y/n: ")
-if answer == "y":
-    cursor.execute(query)
-    results = cursor.fetchall()
-    with open("" + parameter + ".csv", "w") as file:
-        for row in results:
-            csv.writer(file).writerow(row)
-    print()
-    print("A csv document successfully created")
-else:
-    print("Ok, they are not saved")
+
+
+def save():
+    answer = input("Do you want to save the results in csv document? y/n: ")
+    if answer == "y":
+        cursor.execute(query)
+        results = cursor.fetchall()
+        with open("" + parameter + ".csv", "w") as file:
+            for row in results:
+                csv.writer(file).writerow(row)
+        print()
+        print("A csv document successfully created")
+    else:
+        print("Ok, they are not saved. Bye!")
+
+
+save()
 
 # export to csv with header
-annoing = input("Maybe with header y/n: ")
-if annoing == "y":
-    sql_query = pd.read_sql_query(query, con)
-    df = pd.DataFrame(sql_query)
-    # place 'r' before the path name to avoid any errors in the path
-    df.to_csv(r'interactive' + parameter + '.csv', index=False)
-    print("A csv document with header successfully created")
-else:
-    print("Ok, ok. Buy!")
+
+
+def with_header():
+    annoing = input("Maybe with header? y/n: ")
+    if annoing == "y":
+        sql_query = pd.read_sql_query(query, con)
+        df = pd.DataFrame(sql_query)
+        # place 'r' before the path name to avoid any errors in the path
+        df.to_csv(r'interactive' + parameter + '.csv', index=False)
+        print("A csv document with header successfully created")
+    else:
+        print("Ok, ok. Buy!")
+
+
+with_header()
