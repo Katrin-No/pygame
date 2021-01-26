469503363f5ddbbc617d1508d2eb43abaeb1f362
Ekaterina Novikova <ekaterina.novikova@telefonica.com>
2021-01-26 19:55:56 +0100

    def

--- till3block/console.py
+++ till3block/console.py
@@ -1,78 +1,67 @@
-import psycopg2, csv
+import psycopg2
+import csv
 import pandas as pd
 import argparse
 
-con = psycopg2.connect(database="aland", user="azubi", password="a1t1AZUBI", host="localhost", port="5432")
-print("Database opened successfully", end='\n\n')
 
-cursor = con.cursor()
+def connection():
+    global con
+    con = psycopg2.connect(database="aland", user="azubi",
+                           password="a1t1AZUBI", host="localhost", port="5432")
+    print("Database opened successfully", end='\n\n')
 
-# Initialize the parser
-parser = argparse.ArgumentParser()
-# Add the parameters
-parser.add_argument("parameter", help="Type 1)name of continent, 2)first letter of land or 3)all - for the hole table, ", type=str)
-# Parse the arguments
-args = parser.parse_args()
 
-if len(args.parameter) <3:
-    query = "SELECT * FROM land WHERE name LIKE \'" + args.parameter + "%\'"
-elif len(args.parameter) == 3:
-    query = "SELECT * FROM land"
-else:
-    query = "SELECT * FROM land WHERE kontinent = \'" + args.parameter + "\'"
-print("Query is: " + query)
-print("Here the results:")
+connection()
 
-# Durchlaufen der Ergebnisse
-cursor.execute(query)
-row = cursor.fetchone()
-while (row!=None):
-    print(row)
-    row = cursor.fetchone()
 
-#export to csv without header
-cursor.execute(query)
-results = cursor.fetchall()
-with open("" + args.parameter + ".csv", "w") as file:
-    for row in results:
-        csv.writer(file).writerow(row)
-print()
-print("A csv document successfully created")
+def parameters():
+    global args, query
+    # Initialize the parser
+    parser = argparse.ArgumentParser()
+    # Add the parameters
+    parser.add_argument(
+        "parameter", help="Type 1)name of continent, 2)first letter of land or 3)all - for the hole table, ", type=str)
+    # Parse the arguments
+    args = parser.parse_args()
 
-# interactive
-def interactive(parameter):
-    parameter = ""
-    parameter = input("Type:\n - name of continent or \n - first letter of land (A-D) or \n - 'all' for the hole table\n: ")
-    print()
-    if len(parameter) <3:
-        query = "SELECT * FROM land WHERE name LIKE \'" + parameter + "%\'"
-    elif len(parameter) == 3:
+    if len(args.parameter) < 3:
+        query = "SELECT * FROM land WHERE name LIKE \'" + args.parameter + "%\'"
+    elif len(args.parameter) == 3:
         query = "SELECT * FROM land"
     else:
-        query = "SELECT * FROM land WHERE kontinent = \'" + parameter + "\'"
+        query = "SELECT * FROM land WHERE kontinent = \'" + args.parameter + "\'"
     print("Query is: " + query)
-    print("Here the results:")
 
-    # Durchlaufen der Ergebnisse
+
+parameters()
+
+# Durchlaufen der Ergebnisse
+
+
+def your_results():
+    global cursor, row
+    print("Here the results:")
+    cursor = con.cursor()
     cursor.execute(query)
     row = cursor.fetchone()
-    while (row!=None):
+    while (row != None):
         print(row)
         row = cursor.fetchone()
 
-    #export to csv without header
-    def save_csv(answer):
-        answer= input("Do you want to save the results in csv document? y/n: ")
-        if answer == "y":
-            cursor.execute(query)
-            results = cursor.fetchall()
-            with open("" + parameter + ".csv", "w") as file:
-                for row in results:
-                    csv.writer(file).writerow(row)
-            print()
-            print("A csv document successfully created")
-        else:
-            print("Ok, they are not saved. Bye!")
-    save_csv("y")
 
-interactive("B")
+your_results()
+
+# export to csv without header
+
+
+def save_without_header():
+    cursor.execute(query)
+    results = cursor.fetchall()
+    with open("" + args.parameter + ".csv", "w") as file:
+        for row in results:
+            csv.writer(file).writerow(row)
+    print()
+    print("A csv document successfully created")
+
+
+save_without_header()
